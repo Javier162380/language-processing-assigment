c14293c039c87b8c19b10971ddfa1081b9983d31
Javier162380 <javierllorente16@gmail.com>
2018-03-14 21:57:21 +0100

    rank adjusted ok 0.91

--- News_Recognition_assigment/NewsClustering.py
+++ News_Recognition_assigment/NewsClustering.py
@@ -1,13 +1,8 @@
-import re, pprint, os, numpy
+import os, numpy
 import nltk
 from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-from sklearn.metrics.cluster import *
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics.cluster import adjusted_rand_score
-from nltk.corpus import wordnet
-import string
-
 
 def filter_stopwords(token_list):
     stop = set(stopwords.words('english'))
@@ -17,64 +12,28 @@ def filter_stopwords(token_list):
             clean_tokens.append(token)
     return clean_tokens
 
-def wordnet_value(value):
-    if value.startswith('J'):
-        return wordnet.ADJ
-    elif value.startswith('V'):
-        return wordnet.VERB
-    elif value.startswith('N'):
-        return wordnet.NOUN
-    elif value.startswith('R'):
-        return wordnet.ADV
-    else:
-        return None
-
-def lemmatize(token_list):
-    lemmatizer = WordNetLemmatizer()
-    result = []
-    for token in token_list:
-        if len(token) > 0:
-            pos = wordnet_value(token[1])
-            if pos !=None:
-                lemma = lemmatizer.lemmatize(str(token[0]).lower(), pos=pos)
-                result.append(lemma)
-    return result
-
-def remove_punctuation(token_list):
-    result = []
-    for token in token_list:
-        if len(token)>0:
-            punct_removed = ''.join([letter for letter in token if letter in string.ascii_letters])
-            if punct_removed != '':
-                result.append(punct_removed)
-    return result
-
 def process_text(text):
-    tokens = nltk.word_tokenize(text)
-    tokens_tags = nltk.pos_tag(tokens)
-    tokens_lemmatize = lemmatize(tokens_tags)
-    tokens_filter_stop_words = filter_stopwords(tokens)
-    tokens_clean = remove_punctuation(tokens_filter_stop_words)
-    return tokens_clean
+    chunk_tokens =  nltk.ne_chunk(nltk.pos_tag(text.split()))
+    nltk_chunks = [" ".join(w for w, t in elt) for elt in chunk_tokens if isinstance(elt, nltk.Tree)
+                   and hasattr(elt, 'label') and (elt.label()=='PERSON'
+                   or elt.label()=='GPE' or elt.label()=='GPS')]
+    nltk_chunks_erase_stop_words = filter_stopwords(nltk_chunks)
+    return nltk_chunks_erase_stop_words
 
 def cluster_texts(texts, clustersNumber, distance):
     #Load the list of texts into a TextCollection object.
     collection = nltk.TextCollection(texts)
     print("Created a collection of {0}, terms.".format(len(collection)))
-
     #get a list of unique terms
     unique_terms = list(set(collection))
     print("Unique terms found: ", len(unique_terms))
-
     ### And here we actually call the function and create our array of vectors.
     vectors = [numpy.array(TF(f,unique_terms, collection)) for f in texts]
     print("Vectors created.")
-
     # initialize the clusterer
     clusterer = AgglomerativeClustering(n_clusters=clustersNumber,
                                       linkage="average", affinity=distance)
     clusters = clusterer.fit_predict(vectors)
-
     return clusters
 
 # Function to create a TF vector for one document. For each of
@@ -83,7 +42,7 @@ def cluster_texts(texts, clustersNumber, distance):
 def TF(document, unique_terms, collection):
     word_tf = []
     for word in unique_terms:
-        word_tf.append(collection.tf(word, document))
+        word_tf.append(collection.tf_idf(word,document))
     return word_tf
 
 def main():
@@ -91,31 +50,29 @@ def main():
     abs_path=os.path.dirname(os.path.realpath(__file__))+folder
     # Empty list to hold text documents.
     texts = []
+    texts_raw = []
     listing = os.listdir(abs_path)
-    for file in listing:
-        # print("File: ",file)
+    for file in sorted(listing):
         if file.endswith(".txt"):
             url = abs_path+file
             f = open(url, encoding="utf-8" ,errors='ignore')
             raw = f.read()
             f.close()
             texts.append(process_text(raw))
-    print(texts)
+            texts_raw.append(raw)
 
     print("Prepared {0},  documents...".format(len(texts)))
-    print("They can be accessed using texts[0] - texts[ {0}".format(str(len(texts) - 1)))
-#
-    distanceFunction = "cosine"
-    # distanceFunction = "euclidean"
-    test = cluster_texts(texts, 5, distanceFunction)
-    print("test: ", test)
-    # Gold Standard
-    reference = [0, 5, 0, 0, 0, 2, 2, 2, 3, 5, 5, 5, 5, 5, 4, 4, 4, 4, 3, 0, 2, 5]
-    print("reference: ", reference)
+    print("They can be accessed using texts[0] - texts[ {0}".format(str(len(texts) - 1))+"]")
+    distanceFunctions = ["euclidean","braycurtis","cosine"]
 
-    # Evaluation
-    print("rand_score: ", adjusted_rand_score(reference, test))
-
+    for i in distanceFunctions:
+        test = cluster_texts(texts, 5, i)
+        print("test: ", test)
+        ## Gold Standard
+        reference = [0, 5, 0, 0, 0, 2, 2, 2, 3, 5, 5, 5, 5, 5, 4, 4, 4, 4, 3, 0, 2, 5]
+        print("reference: ", reference)
+        ## Evaluation
+        print("resultados para la distancia {0}".format(i))
+        print("rand_score: ", adjusted_rand_score(reference, test))
 if __name__ == "__main__":
     main()
-
